ad3b1e28ffb3a8319fe04cb0fd283b02dd75b41c
Natalie Fearnley <nfearnley@gmail.com>
2020-05-27 13:35:57 -0400

    Strip formatting characters from nick

--- relay.py
+++ relay.py
@@ -8,11 +8,13 @@ http://sopel.chat
 from __future__ import unicode_literals, absolute_import, print_function
 import functools
 import logging
+import re
 
 from sopel.module import rule, require_chanmsg
 from sopel.trigger import PreTrigger
 
 LOGGER = logging.getLogger("sopel.relay")
+FORMATTING_RE = re.compile(r"\x1f|\x02|\x12|\x0f|\x16|\x03(?:\d{1,2}(?:,\d{1,2})?)?")
 
 
 def require_user(*users):
@@ -33,5 +35,8 @@ def require_user(*users):
 def keyword_match(bot, trigger):
     channel = trigger.sender
     nick, message = trigger.groups()
+    nick = FORMATTING_RE.sub('', nick)
+    LOGGER.debug("NICK")
+    LOGGER.debug(f"{nick}")
     pretrigger = PreTrigger(bot.nick, f":{nick}!{nick}@discord.com PRIVMSG {channel} :{message}")
     bot.dispatch(pretrigger)
